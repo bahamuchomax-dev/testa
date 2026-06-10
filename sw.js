82abfd7e3bbf1c84404075afd85dc3d9c571a813
bahamuchomax-dev <bahamuchomax@gmail.com>
2026-06-11 02:24:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - same-origin GET (manifest, etc.): stale-while-revalidate
    - everything else (Firestore/Firebase/Google APIs): not intercepted -> normal network
 */
-var VERSION = 'v5.20.0';
+var VERSION = 'v5.20.1';
 var SHELL = 'oriex-shell-' + VERSION;
 var FONTS = 'oriex-fonts-v1';
 var SHELL_URLS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
